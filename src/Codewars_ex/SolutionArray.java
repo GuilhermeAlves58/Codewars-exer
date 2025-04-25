deb7d068e54940952438fcdb83c613e6ecc11949
perkles <guilhermealves5194@gmail.com>
2025-04-25 11:24:03 -0300

    adding more info

--- src/Codewars_ex/SolutionArray.java
+++ src/Codewars_ex/SolutionArray.java
@@ -1,5 +1,9 @@
 package Codewars_ex;
-
+///You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+///
+/// a can contain numbers or strings. x can be either.
+///
+/// Return true if the array contains the value, false if not.
 public class SolutionArray {
     public static boolean contains(Object[] a, Object x){
         for (Object item : a){
