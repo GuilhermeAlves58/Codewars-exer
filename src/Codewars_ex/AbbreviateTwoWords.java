522c31aa107d0eaafa9243a714f7837005dc3978
perkles <guilhermealves5194@gmail.com>
2025-04-27 22:08:20 -0300

    Daily study

---
+++ src/Codewars_ex/AbbreviateTwoWords.java
@@ -0,0 +1,8 @@
+package Codewars_ex;
+
+public class AbbreviateTwoWords {
+    public static String abbrevName(String name) {
+            String[] parts = name.split(" ");
+            return (parts[0].charAt(0) + "." + parts[1].charAt(0)).toUpperCase();
+    }
+}
