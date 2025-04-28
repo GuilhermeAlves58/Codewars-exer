f849d1e81017bda378acee7e7eecaf6c1741da69
perkles <guilhermealves5194@gmail.com>
2025-04-28 16:54:39 -0300

    .

--- src/Codewars_ex/AbbreviateTwoWords.java
+++ src/Codewars_ex/AbbreviateTwoWords.java
@@ -1,4 +1,13 @@
 package Codewars_ex;
+/// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+///
+/// The output should be two capital letters with a dot separating them.
+///
+/// It should look like this:
+///
+/// Sam Harris => S.H
+///
+/// patrick feeney => P.F
 
 public class AbbreviateTwoWords {
     public static String abbrevName(String name) {
