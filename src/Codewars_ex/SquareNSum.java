d2846c5da160d89f04bb9ec8ebf88244cc5933ad
perkles <guilhermealves5194@gmail.com>
2025-05-05 17:57:25 -0300

    daily study

---
+++ src/Codewars_ex/SquareNSum.java
@@ -0,0 +1,16 @@
+package Codewars_ex;
+
+public class SquareNSum {
+    public static int squareSum(int[] numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num * num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] example = {1, 2, 2};
+        System.out.println(squareSum(example)); //
+    }
+}
