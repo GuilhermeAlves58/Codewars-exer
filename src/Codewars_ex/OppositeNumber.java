cafd0e7ad4a846fa0cd6005309861b1103cba9d0
perkles <guilhermealves5194@gmail.com>
2025-05-07 17:31:35 -0300

    daily study

---
+++ src/Codewars_ex/OppositeNumber.java
@@ -0,0 +1,13 @@
+package Codewars_ex;
+/// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+///
+/// Examples:
+///
+/// 1: -1
+/// 14: -14
+/// -34: 34
+public class OppositeNumber {
+    public static int opposite(int number) {
+        return -number;
+    }
+}
