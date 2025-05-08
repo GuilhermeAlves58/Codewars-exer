4a368af90bd712fe1c53db90eee640da1ee50a10
perkles <guilhermealves5194@gmail.com>
2025-05-08 17:54:28 -0300

    daily study

---
+++ src/Codewars_ex/EvenOrOdd.java
@@ -0,0 +1,7 @@
+package Codewars_ex;
+/// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+public class EvenOrOdd {
+    public static String evenOrOdd(int number) {
+        return (number % 2 == 0) ? "Even" : "Odd";
+    }
+}
