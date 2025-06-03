d6a1ce67fa14c8dc2d5ef0cf55f0da33ac6a72b5
perkles <guilhermealves5194@gmail.com>
2025-06-03 18:11:53 -0300

    daily study

--- src/Codewars_ex/rankUp/DeadFish.java
+++ src/Codewars_ex/rankUp/DeadFish.java
@@ -1,4 +1,49 @@
 package Codewars_ex.rankUp;
 
-public class DeadFish {
+import java.util.ArrayList;
+import java.util.List;
+
+    public class DeadFish {
+        public static int[] parse(String data) {
+            int value = 0;
+            List<Integer> output = new ArrayList<>();
+
+            for (char command : data.toCharArray()) {
+                switch (command) {
+                    case 'i':
+                        value++;
+                        break;
+                    case 'd':
+                        value--;
+                        break;
+                    case 's':
+                        value *= value;
+                        break;
+                    case 'o':
+                        output.add(value);
+                        break;
+                    // Other characters are ignored
+                }
+            }
+
+            // Convert List<Integer> to int[]
+            return output.stream().mapToInt(i -> i).toArray();
+        }
+
+        // Optional main method for testing
+        public static void main(String[] args) {
+            int[] result1 = parse("iiisdoso"); // Should print [8, 64]
+            int[] result2 = parse("iiisdosodddddiso"); // Should print [8, 64, 3600]
+
+            for (int i : result1) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+            for (int i : result2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
 }
